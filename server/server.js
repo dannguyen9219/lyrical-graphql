8a9037bdf920bfea20a031f4b982b01a6e6dac64
nguyen.danimal@gmail.com <nguyen.danimal@gmail.com>
2022-07-07 11:30:18 -0500

    connected to MongoDB Atlus lyricalgraphql project

--- server/server.js
+++ server/server.js
@@ -3,19 +3,26 @@ const models = require('./models');
 const { graphqlHTTP }= require('express-graphql');
 const mongoose = require('mongoose');
 const schema = require('./schema/schema');
+require('dotenv').config();
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_PORT = 27017;
-const MONGO_URI = `mongodb://0.0.0.0:${MONGO_PORT}/lyrical_graphql`;
+// const MONGO_PORT = 27017;
 
-if (!MONGO_URI) {
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+// const MONGO_URI = `mongodb+srv://dannguyen9219:PiALF14gBNsaCZ13@lyricalgraphql.yozehjv.mongodb.net/?retryWrites=true&w=majority`;
+
+if (!DATABASE_URL) {
   throw new Error('You must provide a MongoDB URI!');
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(DATABASE_URL, CONFIG);
 mongoose.connection
     .once('open', () => console.log('Connected to MongoDB instance.'))
     .on('error', error => console.log('Error connecting to MongoDB instance:', error));
